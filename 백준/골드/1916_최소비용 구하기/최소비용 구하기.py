2a85c77d70a01ea5c437e32ef2d59d294e3bb697
inmonim <cat1083014@gmail.com>
2024-03-27 17:28:16 +0900

    1916 최소비용 구하기

---
+++ 백준/골드/1916_최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,36 @@
+import sys, os
+
+path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(path)
+
+from module import stdin_time
+
+@stdin_time
+def sol(input):
+    
+    import heapq
+
+    N, M = int(input()), int(input())
+
+    node = [[] for _ in range(N+1)]
+    for _ in range(M):
+        s, e, v = map(int, input().split())
+        node[s].append((v, e))
+
+    S, E = map(int, input().split())
+
+    visit = [float('inf')] * (N+1)
+    Q = []
+    heapq.heappush(Q, (0, S))
+
+    while Q:
+        v, i = heapq.heappop(Q)
+        if v >= visit[E]:
+            continue
+        for nv, ni in node[i]:
+            rv = nv + v
+            if visit[ni] > rv:
+                visit[ni] = rv
+                heapq.heappush(Q, (rv, ni))
+
+    print(visit[E])
--- 백준/실버/1629_곱셈/곱셉.py
+++ 백준/실버/1629_곱셈/곱셉.py
@@ -9,16 +9,43 @@ from module import stdin_time
 def sol(input):
 
     a, b, c = map(int, input().split())
-    
+
     def dac(a, b, c):
         
         if b == 1:
             return a % c
-        elif not b % 2:
-            return (dac(a, b//2, c) ** 2) % c
+        mod = dac(a, b//2, c)
+        if not b % 2:
+            return (mod ** 2) % c
         else:
-            return ((dac(a, b//2, c) ** 2) * a) % c
+            return (mod ** 2) % c
         
     print(dac(a,b,c))
     
     # print(pow(a,b,c))
+    
+    # (A * B) mod C == ((A mod C) * (B mod C)) mod C
+    
+    # (a * mod ** 2) % c
+    
+    # (a * (a mod c)^2) mod c
+    
+    # =(a mod c * (a mod c)^2 mod c) mod c
+    
+    # =(a mod c * (a mod c * a mod c) mod c) mod c
+    
+    # =(a mod c * (a * a) mod c) mod c
+    
+    # =(a mod c * a^2 mod c) mod c
+    
+    # =(a * a^2) mod c
+    
+    # =a^3 mod c
+    
+    
+    # (a%c * mod ** 2) % c
+    
+    # = (a mod c * (a mod c)^2) mod c
+    # = (a mod c * a mod c * a mod c) mod c
+    # = (a mod c)^3 mod c
+    # = a^3 mod c
